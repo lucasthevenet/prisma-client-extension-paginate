69a650af011ad645f09750eaebbcf99e0ceb0a10
Lucas Thevenet Cervantes <lucastcervantes@gmail.com>
2023-08-10 12:09:29 -0300

    added paginate

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,67 @@
-import { Prisma } from '@prisma/client/extension'
+import { Prisma } from "@prisma/client/extension";
 
-type Args = {}
+const paginate = Prisma.defineExtension({
+  name: "prisma-extension-paginate",
+  model: {
+    $allModels: {
+      async findManyPaginated<T, A>(
+        this: T,
+        args: Prisma.Exact<A, Prisma.Args<T, "findMany">> & {
+          page?: number;
+          take?: never;
+          skip?: never;
+          limit?: number;
+        }
+      ): Promise<{
+        data: Prisma.Result<T, A, "findMany">;
+        meta: {
+          total: number;
+          lastPage: number;
+          currentPage: number;
+          limit: number;
+          prev: number | null;
+          next: number | null;
+        };
+      }> {
+        // @ts-expect-error
+        const { page = 1, limit = 10, ...rest } = args;
+        const ctx = Prisma.getExtensionContext(this);
 
-export const existsFn = (_extensionArgs: Args) =>
-  Prisma.defineExtension({
-    name: "prisma-extension-find-or-create",
-    model: {
-      $allModels: {
-        async exists<T, A>(
-          this: T,
-          args: Prisma.Exact<A, Prisma.Args<T, 'findFirst'>>
-        ): Promise<boolean> {
+        const skip = page > 0 ? limit * (page - 1) : 0;
+        const [total, data] = await Promise.all([
+          // @ts-expect-error
+          ctx.count(
+            // @ts-expect-error
+            args.where
+              ? {
+                  // @ts-expect-error
+                  where: args.where,
+                }
+              : {}
+          ),
+          // @ts-expect-error
+          ctx.findMany({
+            ...rest,
+            take: limit,
+            skip,
+          }),
+        ]);
+        const lastPage = Math.ceil(total / limit);
 
-          const ctx = Prisma.getExtensionContext(this)
-          const result = await (ctx as any).findFirst(args)
-          return result !== null
-        },
+        return {
+          data,
+          meta: {
+            total,
+            lastPage,
+            currentPage: page,
+            limit,
+            prev: page > 1 ? page - 1 : null,
+            next: page < lastPage ? page + 1 : null,
+          },
+        };
       },
     },
-  })
+  },
+});
+
+export default paginate;
